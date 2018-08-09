2539bb0d3b51d3aa5cba175bef7941dfc03f7646
ecs5000@hotmail.com <ecs5000@hotmail.com>
2018-08-08 23:40:03 -0400

    login completado

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 
@@ -8,8 +8,15 @@ def signup(request):
     if request.method == 'POST':
         #User have info and want an account now
         if request.POST['password'] == request.POST['cpassword']:
-            user = User.object.get(username = request.POST['username'])
-            return render(request,'accounts/signup.html', {'error':'Username has already been taken'})
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request,'accounts/signup.html', {'error':'Username has already been taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'],password=request.POST['password'])
+                auth.login(request,user)
+                return redirect('home')
+        else:
+                return render(request,'accounts/signup.html', {'error':'Password must match'})
     else:
     #User wants to enter info
 
@@ -18,9 +25,21 @@ def signup(request):
 
 
 def login(request):
-    return render(request,'accounts/login.html')
+        if request.method == 'POST':
+            user = auth.authenticate(username=request.POST['username'],password=request.POST['password'])
+            if user is not None:
+                auth.login(request,user)
+                return redirect('home')
+            else:
+                return render(request,'accounts/login.html',{'error':'Username or password is incorrect.'})
+        else:
+
+            return render(request,'accounts/login.html')
 
 
 def logout(request):
-    return render(request,'accounts/signup.html')
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
+    
 # Create your views here.
