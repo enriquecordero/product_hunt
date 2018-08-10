b7281cef3518c29158c20a63148f8a6f02498848
ecs5000@hotmail.com <ecs5000@hotmail.com>
2018-08-10 00:27:16 -0400

    details done

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render , redirect , get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Products
 from django.utils import timezone
@@ -25,10 +25,17 @@ def create(request):
             products.pub_date = timezone.datetime.now()
             products.hunter = request.user
             products.save()
-            return redirect('home')
+            return redirect('/products/' + str(products.id))
 
         else:
             return render(request, 'products/create.html',{'error':'All field are required '})
     else:
         return render(request, 'products/create.html')
+
+
+def detail(request,product_id):
+
+    product = get_object_or_404(Products ,pk=product_id)
+    return render(request, 'products/detail.html' , {'product':product})
+
 # Create your views here.
